2ef21e52970b478da884fcb8d916026f794fb8db
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-05-28 19:40:08 +0200

    RMSNorm is added in the src

--- src/RMSNorm.py
+++ src/RMSNorm.py
@@ -5,19 +5,31 @@ import torch.nn as nn
 
 sys.path.append("./src/")
 
+
 class RMSNormalization(nn.Module):
     def __init__(self, dimension: int = 512, eps: float = 1e-4):
         super(RMSNormalization, self).__init__()
-        
+
         self.dimension = dimension
         self.eps = eps
-        
+
+        self.gamma = nn.Parameter(data=torch.ones((self.dimension // self.dimension,
+                                  self.dimension // self.dimension, self.dimension)), requires_grad=True)
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor".capitalize())
+
+        RMS = torch.sqrt(torch.mean(x ** 2, dim=-1) + self.eps)
+        RMS = RMS.unsqueeze(dim=-1)
         
-        pass
-    
-    
+        RMSNorm = x / RMS
+        
+        return torch.mul(RMSNorm, self.gamma)
+        
+
+
 if __name__ == "__main__":
-    pass
+    norm = RMSNormalization(dimension=512)
+    
+    print(norm(torch.randn(64, 128, 512)).size())
