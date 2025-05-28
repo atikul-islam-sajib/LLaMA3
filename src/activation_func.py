9b38a9bd110364d1fdbb4fe8b1e26a8068f1accf
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-05-28 15:56:27 +0200

    SwiGLU is added

--- src/activation_func.py
+++ src/activation_func.py
@@ -1,18 +1,30 @@
 import os
 import sys
+import math
 import torch
 import torch.nn as nn
 
 sys.path.append("./src/")
 
+
 class SwiGLU(nn.Module):
     def __init__(self, name: str = "SwiGLU"):
         super(SwiGLU, self).__init__()
-        
+
+        self.name = name
+        self.constant = 0.044715
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor")
-        pass
-    
+
+        swish = x * torch.sigmoid(x)
+        gelu = 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2 / math.pi)) * (x + self.constant * torch.pow(x, 3))))
+        return swish * gelu
+
+
 if __name__ == "__main__":
-    pass
+    activation_func = SwiGLU()
+
+    texts = torch.randn((64, 128, 512))
+    print(activation_func(texts).size())
