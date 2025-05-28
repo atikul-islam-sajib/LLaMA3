dc3262c1e386bff1dab3f2594884dcae60c6070f
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-05-28 16:02:56 +0200

    unittest is defined for the activation func

---
+++ unittest/test.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import torch
+import unittest
+import torch.nn as nn
+
+sys.path.append("./src/")
+
+from activation_func import SwiGLU
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.batch_size = 64
+        self.sequence_length = 128
+        self.dimension_size = 512
+
+        self.activation_func = SwiGLU()
+
+    def test_activation_func(self):
+        texts = torch.randn((self.batch_size, self.sequence_length, self.dimension_size))
+
+        self.assertEqual(self.activation_func(texts).shape, texts.shape,
+                         "SwiGLU activation function is not working properly".capitalize())
+        
+        self.assertIsInstance(self.activation_func(texts), torch.Tensor,
+                              "SwiGLU activation function is not working properly".capitalize())
+        
+if __name__ == "__main__":
+    unittest.main()
