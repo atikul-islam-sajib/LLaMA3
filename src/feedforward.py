0aa89307aeac08b0bb455d49d998e3441f4b38dc
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-05-29 15:55:38 +0200

    CLI is added in the src

--- src/feedforward.py
+++ src/feedforward.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import torch
+import argparse
 import torch.nn as nn
 
 sys.path.append("./src/")
@@ -51,6 +52,37 @@ class FeedForwardNeuralNetwork(nn.Module):
 
 
 if __name__ == "__main__":
-    network = FeedForwardNeuralNetwork(hidden_dimension=512, output_dimension=4 * 512)
-    texts = torch.randn((64, 128, 512))
-    print(network(texts).size())
+    parser = argparse.ArgumentParser(
+        description="MLP layer with SwiGLU activation function"
+    )
+    parser.add_argument(
+        "--hidden_dimension",
+        type=int,
+        default=4096,
+        help="Dimension of the hidden layer".capitalize(),
+    )
+    parser.add_argument(
+        "--output_dimension",
+        type=int,
+        default=14336,
+        help="Dimension of the output layer".capitalize(),
+    )
+    args = parser.parse_args()
+
+    hidden_dimension = args.hidden_dimension
+    output_dimension = args.output_dimension
+
+    batch_size = 64
+    sequence_length = 128
+
+    network = FeedForwardNeuralNetwork(
+        hidden_dimension=hidden_dimension, output_dimension=output_dimension
+    )
+
+    texts = torch.randn((batch_size, sequence_length, hidden_dimension))
+
+    assert (network(texts).size()) == (
+        batch_size,
+        sequence_length,
+        hidden_dimension,
+    ), "FeedForwardNeuralNetwork is not working properly".capitalize()
